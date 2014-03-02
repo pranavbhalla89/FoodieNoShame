0cb2fac2adc47a5e3c5cb59b7b8e6298049904eb
pranavbhalla89 <Apple@dyn-160-39-216-147.dyn.columbia.edu>
2014-03-02 17:45:39 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodieNoShame::Application.config.secret_key_base = 'c46613c1ea0091479c3cce856e957b5f5d01fa2da6c169240f6f6be61bc5ceade777814d1c9790be1623b09fb0f4d86fbf90e893e82b7bd22899f00838bbf7d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FoodieNoShame::Application.config.session_store :cookie_store, key: '_FoodieNoShame_session'
